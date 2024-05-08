c1906abb12f460a8a8266c9d6ea630cf3086f92e
ryanhanks-bestow <87093311+ryanhanks-bestow@users.noreply.github.com>
2024-05-07 22:49:34 -0500

    feat: use asset reader with scratch space to provide assets to buf build (#2)
    * feat: use asset reader with scratch space to provide assets to buf build
    
    * chore: bump version

--- test/src/run_buf_build_test.dart
+++ test/src/run_buf_build_test.dart
@@ -1,16 +1,27 @@
-import 'dart:io';
-
+import 'package:build_test/build_test.dart';
 import 'package:flakka_buf_runner/flakka_buf_runner.dart';
+import 'package:glob/glob.dart';
 import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
 void main() {
-  group('buf build', () {
-    test('buf build', () async {
-      final targetDirectory =
-          p.join(Directory.current.path, 'example', 'proto', 'module1');
-      final result = await runBufBuild(targetDirectory);
-      expect(result, isNotEmpty);
-    });
+  test('buf build', () async {
+    final assetReader = await PackageAssetReader.currentIsolate(
+      rootPackage: 'flakka_buf_runner',
+    );
+
+    final bufYamlAssetIds =
+        await assetReader.findAssets(Glob('test_proto/**/buf.yaml')).toList();
+    assert(bufYamlAssetIds.isNotEmpty, '');
+
+    final targetDirectory = p.joinAll(
+      bufYamlAssetIds.single.pathSegments
+          .skip(bufYamlAssetIds.first.pathSegments.length - 3)
+          .take(2),
+    );
+
+    final result = await runBufBuild2(assetReader, targetDirectory);
+
+    expect(result, isNotEmpty);
   });
 }
